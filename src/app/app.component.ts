da371c0de2e1fc0fe30de48a4953a74ec7d380a1
MuzammilAmjad <muzammilamjad217@gmail.com>
2024-11-29 18:54:12 +0500

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {WebcrawlComponent} from '../webcrawl/webcrawl.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [WebcrawlComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
